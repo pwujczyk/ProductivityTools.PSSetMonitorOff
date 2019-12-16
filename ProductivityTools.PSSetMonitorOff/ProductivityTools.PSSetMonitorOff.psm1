c777a02596159c3ed3689a50fac06939791c0acd
pwujczyk <pwujczyk@ecovadis.com>
2019-12-16 22:05:21 +0100

    working app

--- ProductivityTools.PSSetMonitorOff/ProductivityTools.PSSetMonitorOff.psm1
+++ ProductivityTools.PSSetMonitorOff/ProductivityTools.PSSetMonitorOff.psm1
@@ -1,6 +1,24 @@
-<#
-	My Function
-#>
-function Get-Function {
+function Set-MonitorOff ()
+{
+	[CmdletBinding()]
+	param()
 
+	$HWND = -1
+	$WM_SYSCOMMAND = 0x0112
+	$SC_MONITORPOWER = 0xF170
+	$MONITOR_ON = -1
+	$MONITOR_OFF = 2
+ 
+	#Store the C# signature of the SendMessage function. 
+	$signature = '[DllImport("user32.dll")] public static extern int PostMessage(int hWnd, int hMsg, int wParam, int lParam);'
+ 
+	#Add the SendMessage function as a static method of a class
+	$SendMessage = Add-Type -MemberDefinition $signature -Name "Win32SendMessage" -Namespace Win32Functions -PassThru
+ 
+	Write-Verbose "Invoke Monitor Off"
+	#Invoke the SendMessage Function
+	$SendMessage::PostMessage($HWND, $WM_SYSCOMMAND, $SC_MONITORPOWER, $MONITOR_OFF) |Out-Null
+ 
+	Write-Verbose "Monitor off"
 }
+Export-ModuleMember Set-MonitorOff
---
+++ ProductivityTools.PSSetMonitorOff/Runner.ps1
@@ -0,0 +1,4 @@
+clear 
+Import-Module D:\GitHub\ProductivityTools.PSSetMonitorOff\ProductivityTools.PSSetMonitorOff\ProductivityTools.PSSetMonitorOff.psm1 -Force
+
+Set-MonitorOff -Verbose 
